7e77a198518bf1ff02333bbe93fa4dc915988b9b
Carlota Bindner <cev.bindner@gmail.com>
2017-09-22 18:21:48 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,7 +16,7 @@ class Doctor
   end
 
   def patients
-    self.appointments.collect { |patient| appointments.patient }
+    self.appointments.collect { |patient| appointment.patient }
   end
 
 end
