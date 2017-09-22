fcf1e8adb430d668bc6622e622acd87653a73b40
Carlota Bindner <cev.bindner@gmail.com>
2017-09-22 18:18:38 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,22 @@
+class Doctor
+  attr_accessor :name, :appointments
+
+  def intialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    self.appointments.collect { |patient| appointments.patient }
+  end
+
+end
